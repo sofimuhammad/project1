d2b6822c2378c9bab0a3125164ad3fab9fe6e6bf
m-sofimuhammad <m.sofimuhammad877@gmail.com>
2020-10-23 21:04:47 +0700

    menambahkan file project

---
+++ MyIntentApp/app/src/main/java/com/example/myintentapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myintentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn_move_activity:
+                break;
+        }
+    }
+}
